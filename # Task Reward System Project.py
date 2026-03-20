a49bdcf43e126cb6b32d8ce1d1bbd80a6cbb0cf6
ZA-7 <zahraahuss774@gmail.com>
2026-03-20 05:07:12 +0300

    Added a points system to make the game more interactive

--- # Task Reward System Project.py
+++ # Task Reward System Project.py
@@ -1,7 +1,9 @@
 # Task Reward System Project
 # Project Owner: (Write Your Name Here)
-
 print("--- 🎮 Welcome to the Task Achievement Game 🎮 ---")
+
+# التعديل: إضافة متغير لجمع النقاط
+total_points = 0
 
 task = input("What task did you complete today? ")
 
@@ -15,13 +17,18 @@ choice = input("Enter category number (1, 2, or 3): ")
 if choice == "1":
     print(f"Great job! Since you finished '{task}' in Study..")
     print("Reward: You earned a Blue Gem! 💎")
+    total_points += 50 # أضفتِ 50 نقطة
 elif choice == "2":
     print(f"Awesome! Completing '{task}' in housework deserves a treat..")
     print("Reward: You earned a Chocolate bar! 🍫")
+    total_points += 30 # أضفتِ 30 نقطة
 elif choice == "3":
     print(f"Excellent! Doing '{task}' is a great achievement..")
     print("Reward: You earned 100 Gold Coins! 💰")
+    total_points += 20 # أضفتِ 20 نقطة
 else:
-    print("Invalid choice, please run the program again and choose 1, 2, or 3.")
+    print("Invalid choice!")
 
-print("\nKeep up the great work! 💪")
+# عرض مجموع النقاط النهائي
+print(f"\n🏆 Your current total score is: {total_points} Points!")
+print("Keep up the great work! 💪")
